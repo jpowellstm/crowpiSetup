d509e87b3d6c514385bb96187dc79804a566add8
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2024-10-24 13:16:15 +0100

    General Update added to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+# General Update
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
 # Download and install vscodium
 wget www.github.com/vscodium/vscodium/releases/download/1.84.2.23319/codium_1.84.2.23319_armhf.deb
 sudo dpkg -i codium_1.84.2.23319_armhf.deb
