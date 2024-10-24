bf950d071f90666becd1a9986b36a8040ea7ec02
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2024-10-24 12:50:27 +0100

    Create setup.sh
    Install vscodium

---
+++ setup.sh
@@ -0,0 +1,4 @@
+# Download and install vscodium
+wget www.github.com/vscodium/vscodium/releases/download/1.84.2.23319/codium_1.84.2.23319_armhf.deb
+sudo dpkg -i codium_1.84.2.23319_armhf.deb
+rm codium_1.84.2.23319_armhf.deb
