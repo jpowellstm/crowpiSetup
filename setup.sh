6e75cf4ad6597983a4e6a857fb750830ac0abae4
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2024-12-05 13:24:46 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ sudo apt-get upgrade -y
 wget www.github.com/vscodium/vscodium/releases/download/1.84.2.23319/codium_1.84.2.23319_armhf.deb
 sudo dpkg -i codium_1.84.2.23319_armhf.deb
 rm codium_1.84.2.23319_armhf.deb
+
+# Install extensions
+codium --install-extension https://github.com/microsoft/vscode-cpptools/releases/download/1.3.1/cpptools-linux.vsix
