83c02666b73db049b38c62e2c3a114c16607a3a6
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2024-12-05 14:37:14 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,11 @@ rm codium_1.84.2.23319_armhf.deb
 # Install extensions
 wget https://github.com/microsoft/vscode-cpptools/releases/download/1.3.1cpptools-linux.vsix
 codium --install-extension https://github.com/microsoft/vscode-cpptools/releases/download/1.3.1/cpptools-linux.vsix
+rm cpptools-linux.vsix
+
+wget https://github.com/jpowellstm/crowpiSetup/blob/main/platformio.platformio-ide-3.3.3%40linux-arm64.vsix
+codium --install-extension platformio.platformio-ide-3.3.3%40linux-arm64.vsix
+rm platformio.platformio-ide-3.3.3%40linux-arm64.vsix
 
 
 
